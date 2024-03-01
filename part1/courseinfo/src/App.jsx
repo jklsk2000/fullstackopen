86dce1629e860be17dc4988189355c9e2e925af2
Danny Lee <jklsk2000@gmail.com>
2024-02-29 22:22:53 -0600

    up to part 1d

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 const Header = ({ course }) => <h1>{course}</h1>
 
 const Content = ({ part, exercises }) => <p>{part} {exercises}</p>
@@ -5,27 +6,29 @@ const Content = ({ part, exercises }) => <p>{part} {exercises}</p>
 const Total = ({ total }) => <p>Number of exercises {total}</p>
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const contents = [
-    {
-      part: 'Fundamentals of React',
-      exercises: 10,
-    },
-    {
-      part: 'Using props to pass data',
-      exercises: 7,
-    },
-    {
-      part: 'State of a component',
-      exercises: 14,
-    },
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        part: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        part: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        part: 'State of a component',
+        exercises: 14,
+      },
+    ]
+  }
   let total = 0;
 
   return (
     <>
-      <Header course={course} />
-      {contents.map(({ part, exercises }) => {
+      <Header course={course.name} />
+      {course.parts.map(({ part, exercises }) => {
         total += exercises;
         return (
           <Content key={part} part={part} exercises={exercises} />
